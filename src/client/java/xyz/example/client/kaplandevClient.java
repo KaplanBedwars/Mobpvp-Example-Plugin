1f5028395ca21c27d7c325174696424c7a2549a3
Your Name <df@example.com>
2025-06-30 22:57:55 +0300

    First

---
+++ src/client/java/xyz/example/client/kaplandevClient.java
@@ -0,0 +1,10 @@
+package xyz.example.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class kaplandevClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/xyz/example/kaplandev.java
@@ -0,0 +1,10 @@
+package xyz.example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class kaplandev implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
